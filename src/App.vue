79777cf704710075cfc52e25be710274a55a7d11
faizfajar <faizaprianda112@gmail.com>
2026-02-08 18:43:56 +0700

    remove unused import files

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 import { computed } from 'vue'
 import { useBoothStore } from './store'
 import StartScreen from './views/StartScreen.vue'
-import FrameSelection from './views/FrameSelection.vue'
 import LayoutSelection from './components/LayoutSelection.vue'
 import CameraPreview from './components/CameraPreview.vue'
 import ReviewScreen from './views/ReviewScreen.vue'
@@ -25,13 +24,12 @@ const currentView = computed(() => {
 </script>
 
 <style>
-/* Pastikan ini ada di file CSS global kamu */
 html, body, #app {
   margin: 0;
   padding: 0;
   height: 100%;
   width: 100%;
-  background-color: black; /* Hindari warna putih yang bikin silau */
+  background-color: black;
   overflow: hidden;
 }
 </style>
